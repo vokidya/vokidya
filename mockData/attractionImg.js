29bfefebf02d4936a4e97029998f5c46180ddcec
Minmin Tang <minmin.tang@careerbuilder.com>
2018-03-16 16:33:28 +0800

    add mock data

--- mockData/attractionImg.js
+++ mockData/attractionImg.js
@@ -1,6 +1,7 @@
 var attractionImg = [
   { location: '拙政园', img: 'DSC00006_polarr.jpg'},
   { location: '网师园', img: 'DSC00186_polarr.jpg'},
+  { location: '狮子林', img: 'DSC00233_polarr.jpg' },
 ]
 
 module.exports = {
