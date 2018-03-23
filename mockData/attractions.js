02aaf3766d946c78cf48214e8a49f6731b4c9d83
Shenghua <shenghua.lian@careerbuilder.com>
2018-03-23 14:22:21 +0800

    change data name to atrration

--- mockData/attractions.js
+++ mockData/attractions.js
@@ -1,9 +1,9 @@
-var attractionImg = [
-  { location: '拙政园', img: 'DSC00006_polarr.jpg'},
+var attractions = [
+  { location: '拙政园s', img: 'DSC00006_polarr.jpg'},
   { location: '网师园', img: 'DSC00186_polarr.jpg'},
   { location: '狮子林', img: 'DSC00233_polarr.jpg' },
 ]
 
 module.exports = {
-  attractionImg
+  attractions
 }
