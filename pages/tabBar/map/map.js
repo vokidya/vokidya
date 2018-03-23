7cf265fb5e2d05eee9b6dd5ea14853038a371cf0
Minmin Tang <minmin.tang@careerbuilder.com>
2018-03-23 17:41:29 +0800

    add map for this project

--- pages/tabBar/map/map.js
+++ pages/tabBar/map/map.js
@@ -1,65 +1,66 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    toView: '',
+    scrollTop: 100,
+    scale: 13,
+    latitude: '',
+    longitude: '',
+    markers: [],
+    controls: [{
+      id: 1,
+      iconPath: '../../../assets/images/icon-minus.png',
+      position: {
+        left: 310, 
+        top: 100 - 50,
+        width: 25,
+        height: 25
+      },
+      clickable: true
+    },
+    {
+      id: 2,
+      iconPath: '../../../assets/images/icon-plus.png',
+      position: {
+        left: 340,
+        top: 100 - 50,
+        width: 25,
+        height: 25
+      },
+      clickable: true
+    }
+    ],
+    circles: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  controltap(e) {
+    var that = this;
+    if (e.controlId === 1) { //点击减号的时候触发
+      that.setData({
+        scale: --this.data.scale
+      })
+    } else { // 点击加号的时候触发
+      that.setData({
+        scale: ++this.data.scale
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  onLoad: function () {
+    var _this = this;
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        _this.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          markers: [{
+            latitude: res.latitude,
+            longitude: res.longitude,
+            width: 50,
+            height: 50,
+            iconPath: '../../../assets/images/icon-location.png',
+            title: 'location'
+          }]
+        })
+      }
+    })
   }
 })
