a211640cee1d78ce1c2e1d97adc7856a9f36a0cf
Shenghua <shenghua.lian@careerbuilder.com>
2018-03-23 17:45:39 +0800

    add attration view component

---
+++ components/attraction-view/attraction-view.js
@@ -0,0 +1,57 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name: {
+      type: String,
+      value: ''
+    },
+
+    coverImage: {
+      type: String,
+      value: ''
+    },
+
+    images: {
+      type: Array,
+      value: ''
+    },
+
+    rating: {
+      type: String,
+      value: ''
+    },
+
+    protectLevel: {
+      type: String,
+      value: ''
+    },
+
+    tags: {
+      type: Array,
+      value: ''
+    },
+    
+    dataSource: {
+      type: Object,
+      value: {}
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    clickImage() {
+      this.triggerEvent('clickItem', this.data.dataSource);
+    }
+  }
+})
